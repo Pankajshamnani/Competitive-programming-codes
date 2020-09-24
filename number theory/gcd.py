8bd76886a7454cf1069679521ae2fb2a89b40dfc
Pankaj Shamnani <48305112+Pankajshamnani@users.noreply.github.com>
2020-09-24 11:01:43 +0530

    Update and rename gdc.py to gcd.py

--- number theory/gcd.py
+++ number theory/gcd.py
@@ -5,7 +5,7 @@ def get_int(): return int(sys.stdin.readline().strip())
 def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
 def get_ints(): return map(int, sys.stdin.readline().strip().split())
 
-def gcd(a,b):
+def gcd(a,b):  #time complexity->O(log(min(a,b)))
     if b==0:
         return a
     return gcd(b,a%b)
