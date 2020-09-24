1779bb9d2e8715c7d9f748e08e0f46c9eab8db38
Pankaj Shamnani <48305112+Pankajshamnani@users.noreply.github.com>
2020-09-24 10:59:09 +0530

    Update primes.py

--- number theory/primes.py
+++ number theory/primes.py
@@ -15,6 +15,20 @@ def isprime(n):
             return 0
     return 1
 
+#little better algo to calculate primes
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+    max_div = math.floor(math.sqrt(n))
+    for i in range(3, 1 + max_div, 2):
+        if n % i == 0:
+            return False
+    return True
+
 # code to check prime numbers from 1 to n => limit:10^^6 timecomplexity=>O(nloglogn)
 def sieve(n):
     prime=[True for i in range(n+1)]
