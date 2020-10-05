be6e5869b3bf35bcdba5d383e4aaf0a91ea4356d
Pankaj Shamnani <48305112+Pankajshamnani@users.noreply.github.com>
2020-10-05 10:04:33 +0530

    Update factorisation.py

--- number theory/factorisation.py
+++ number theory/factorisation.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from collections import defaultdict
 def get_string(): return sys.stdin.readline().strip()
 def get_int(): return int(sys.stdin.readline().strip())
 def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
@@ -15,6 +16,25 @@ def factorization(n):
         prime_factors.append(n)
     return prime_factors
 
+def power_factorization(n):
+    prime_factors=defaultdict(lambda: 0)
+    for i in range(2,int(math.sqrt(n))+1):
+        while(n%i==0):
+            prime_factors[i]+=1
+            n=n//i
+    if n>1:
+        prime_factors[n]+=1
+    return prime_factors
+
 
 n=get_int()
 print(factorization(n))
+
+# for dictionary
+ans=power_factorization(n)
+product_of_all_factors=1
+for i in ans:
+    product_of_all_factors*=ans[i]+1
+    print(i,ans[i])
+#     total number of factors is the product of all (ans[i]+1)
+print(product_of_all_factors)
